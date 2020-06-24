d51297236bebd6f2a3b5a3a89fbeb779393f7b77
Neha Kennard <nnayak@cs.umass.edu>
2020-06-24 14:26:28 -0700

    tabs kinda working

--- _includes/display.js
+++ _includes/display.js
@@ -1,7 +1,32 @@
+function makeOneTab(tokens, rel){
+  if (rel[0] > rel[2]){
+    positions = [rel[2], rel[3], rel[0], rel[1]];
+    ns = false;
+  } else {
+    positions = [rel[0], rel[1], rel[2], rel[3]];
+    ns = true;
+  }
+
+  tokens.splice(positions[3], 0, "</span>");
+  if (ns) {
+    tokens.splice(positions[2], 0, `<span class="sat">`);
+  } else {
+    tokens.splice(positions[2], 0, `<span class="nuc">`);
+  }
+  tokens.splice(positions[1], 0, "</span>");
+  if (ns) {
+    tokens.splice(positions[0], 0, `<span class="nuc">`);
+  } else {
+    tokens.splice(positions[0], 0, `<span class="sat">`);
+  }
+
+  return `<div class="tab" style="display:none">` + tokens.join(" ") + "</div>"
+
+}
+
 function makeDisplay(comment_data){
   b = JSON.parse(comment_data);
-  console.log(b);
-  k = document.getElementsByClassName("commentSpace");
+  k = document.getElementById("commentSpace");
   newTokens = b["tokens"];
 
   rel = b["rels"][1];
@@ -15,31 +40,42 @@ function makeDisplay(comment_data){
   relNumSpan = document.getElementById("relNum");
   relNumSpan.innerHTML = 1+"/"+b["rels"].length;
 
-  if (rel[0] > rel[2]){
-    positions = [rel[2], rel[3], rel[0], rel[1]];
-    ns = false;
-  } else {
-    positions = [rel[0], rel[1], rel[2], rel[3]];
-    ns = true;
+
+
+  for (var i=0;i<b["rels"].length; i++){
+    newTab = makeOneTab(b["tokens"], rel)
+    k.innerHTML += newTab;
   }
-
-  tokens = b["tokens"]
-
-  tokens.splice(positions[3], 0, "</span>");
-  if (ns) {
-    tokens.splice(positions[2], 0, `<span class="sat">`);
-  }else{
-    tokens.splice(positions[2], 0, `<span class="nuc">`);
-  }
-  tokens.splice(positions[1], 0, "</span>");
-  if (ns) {
-    tokens.splice(positions[0], 0, `<span class="nuc">`);
-  }else{
-    tokens.splice(positions[0], 0, `<span class="sat">`);
-  }
-
-
-  k.item(0).innerHTML = tokens.join(" ");
+  showTab(0);
 
 }
 
+currentTab = 0;
+
+function showTab(n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName("tab");
+  x[n].style.display = "block";
+  // ... and fix the Previous/Next buttons:
+  if (n == 0) {
+    document.getElementById("prevBtn").style.display = "none";
+  } else {
+    document.getElementById("prevBtn").style.display = "inline";
+  }
+  if (n == (x.length - 1)) {
+    document.getElementById("nextBtn").innerHTML = "Submit";
+  } else {
+    document.getElementById("nextBtn").innerHTML = "Next";
+  }
+  // ... and run a function that displays the correct step indicator:
+}
+
+function nextPrev(n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName("tab");
+  // Hide the current tab:
+  x[currentTab].style.display = "none";
+  // Increase or decrease the current tab by 1:
+  currentTab = (currentTab + n) % x.length;
+  showTab(currentTab);
+}
